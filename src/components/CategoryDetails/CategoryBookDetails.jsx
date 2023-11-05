04234ab2612919f914ef4d39726f6c57261d98f1
2eg4rizve <2eg4rizve.com>
2023-11-05 18:01:19 +0600

    borrow books

--- src/components/CategoryDetails/CategoryBookDetails.jsx
+++ src/components/CategoryDetails/CategoryBookDetails.jsx
@@ -1,24 +1,98 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useLoaderData, useParams } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import Swal from "sweetalert2";
 
 
 const CategoryBookDetails = () => {
     const { _id } = useParams();
     let id = _id;
-    console.log(id);
+    //console.log(id);
 
     const books = useLoaderData();
 
     const [nowBook, setNowBook] = useState();
+
+    const { user } = useContext(AuthContext);
+
 
     useEffect(() => {
         const rec = books?.filter(item => item._id == id)
         setNowBook(rec[0])
     }, [books, id])
 
-    console.log(nowBook);
+    //console.log(nowBook);
 
     const { photo, bookName, quantityOfTheBook, authorName, categoryName, shortDescription, rating } = nowBook || {}
+
+    const currentDate = new Date();
+
+    // Format the date as 'YYYY-MM-DD'
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+
+    // console.log(formattedDate)
+
+
+    let qtob =  quantityOfTheBook ;
+    let cn = categoryName ;
+
+
+
+
+    const handleAdd = event => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const userName = form?.userName?.value;
+        const userEmail = form?.userEmail?.value;
+        const photo = form.photo.value;
+        const bookName = form.bookName.value;
+        const quantityOfTheBook = qtob;
+        const authorName = form.authorName.value;
+        const categoryName = cn;
+        const rating = form.rating.value;
+        const borrowDate = formattedDate;
+        const returnDate = form.returnDate.value;
+
+        // console.log("currentDate : ", formattedDate);
+        // console.log("returnDate : ", returnDate);
+
+
+
+        const newBook = { userName, userEmail, photo, bookName, quantityOfTheBook, authorName, categoryName, rating, borrowDate, returnDate }
+
+        console.log(newBook);
+
+
+
+        fetch('http://localhost:5000/borrowBooks', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newBook)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log("add  : ", data)
+                if (data.insertedId) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Thanks...',
+                        text: 'Book Borrow successfully',
+
+                    })
+                }
+            })
+
+
+    }
+
+
 
     return (
         <div>
@@ -37,7 +111,7 @@ const CategoryBookDetails = () => {
                     <h2 className="card-title pb-[20px]">Rating : {rating} / 5</h2>
 
                     <Link to={`/categoryBookRead/${_id}`}>
-                        <button className="btn btn-primary w-full  mt-[30px]">Read</button>
+                        <button className="btn btn-primary w-full  mt-[30px] mb-[10px]">Read</button>
                     </Link>
 
 
@@ -46,16 +120,124 @@ const CategoryBookDetails = () => {
                     </Link> */}
 
                     {/* Open the modal using document.getElementById('ID').showModal() method */}
-                    <button className="btn" onClick={() => document.getElementById('my_modal_5').showModal()}>Borrow</button>
+                    <button className="btn btn-primary" onClick={() => document.getElementById('my_modal_5').showModal()}>Borrow</button>
                     <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
                         <div className="modal-box">
+
                             <h3 className="font-bold text-lg">Hello!</h3>
-                            <p className="py-4">Press ESC key or click the button below to close Press ESC key or click the button below to close Press ESC key or click the button below to close</p>
-                            <p className="py-4">Press ESC key or click the button below to close</p>
-                            <p className="py-4">Press ESC key or click the button below to close</p>
-                            <p className="py-4">Press ESC key or click the button below to close</p>
-                            <p className="py-4">Press ESC key or click the button below to close</p>
-                            <p className="py-4">Press ESC key or click the button below to close</p>
+
+                            <form onSubmit={handleAdd}>
+
+
+                                {/*User Name  */}
+                                <div className="form-control w-full mb-4">
+                                    <label className="label">
+                                        <span className="label-text">User Name</span>
+                                    </label>
+                                    <label className="input-group">
+                                        <input type="text" name='userName' value={user?.displayName} placeholder="User Name" className="input input-bordered w-full" />
+                                    </label>
+                                </div>
+
+                                {/*User Email  */}
+                                <div className="form-control w-full mb-4">
+                                    <label className="label">
+                                        <span className="label-text">User Email</span>
+                                    </label>
+                                    <label className="input-group">
+                                        <input type="text" name='userEmail' value={user?.email} placeholder="User Name" className="input input-bordered w-full" />
+                                    </label>
+                                </div>
+
+
+
+                                {/* Photo */}
+                                <div className="form-control w-full mb-4">
+                                    <label className="label">
+                                        <span className="label-text">Photo URL</span>
+                                    </label>
+                                    <label className="input-group">
+                                        <input type="text" name='photo' value={photo} placeholder="Photo URL" className="input input-bordered w-full" />
+                                    </label>
+                                </div>
+
+                                {/*Book Name */}
+                                <div className="form-control w-full mb-4">
+                                    <label className="label">
+                                        <span className="label-text">Book Name</span>
+                                    </label>
+                                    <label className="input-group">
+                                        <input type="text" name='bookName' value={bookName} placeholder="Book Name" className="input input-bordered w-full" required />
+                                    </label>
+                                </div>
+
+
+
+                                {/*Author Name */}
+                                <div className="form-control w-full mb-4">
+                                    <label className="label">
+                                        <span className="label-text">Author Name</span>
+                                    </label>
+                                    <label className="input-group">
+                                        <input type="text" name='authorName' value={authorName} placeholder="Author Name" className="input input-bordered w-full" required />
+                                    </label>
+                                </div>
+
+
+                                {/*Category Name */}
+                                <div className="form-control w-full mb-4">
+                                    <label className="label">
+                                        <span className="label-text">Category Name</span>
+                                    </label>
+                                    <label className="input-group">
+                                        <input type="text" name='categoryName' value={categoryName} placeholder="Category Name" className="input input-bordered w-full" required />
+                                    </label>
+                                </div>
+
+
+
+
+                                {/* Rating */}
+                                <div className="form-control w-full mb-4">
+                                    <label className="label">
+                                        <span className="label-text">Rating </span>
+                                    </label>
+                                    <label className="input-group">
+                                        <input type="number" name='rating' value={rating} placeholder="Rating" className="input input-bordered w-full" required />
+                                    </label>
+                                </div>
+
+
+                                {/* Borrow date */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Borrow date</span>
+                                    </label>
+                                    <input type="date" name="borrowDate" value={formattedDate} className="input input-bordered" required />
+
+                                </div>
+
+
+                                {/* Return Date */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Return Date</span>
+                                    </label>
+                                    <input type="date" name="returnDate" className="input input-bordered" required />
+
+                                </div>
+
+
+
+                                <input type="submit" value="Add" className="btn btn-block btn-primary mt-[20px]" />
+
+
+                            </form>
+
+
+
+
+
                             <div className="modal-action">
                                 <form method="dialog">
                                     {/* if there is a button in form, it will close the modal */}
--- src/pages/AddBookPage/AddBookPage.jsx
+++ src/pages/AddBookPage/AddBookPage.jsx
@@ -54,6 +54,8 @@ const AddBookPage = () => {
                 }
             })
     }
+
+    
     return (
         <div>
             <div>
